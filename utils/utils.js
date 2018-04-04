a84836f7c290556bd1a59020add482508c6fa9f2
MarlenaFlueh <mf@esyoil.com>
2018-04-04 21:14:57 +0200

    require expect libary and add new test

--- utils/utils.js
+++ utils/utils.js
@@ -1,2 +1,9 @@
 module.exports.add = (a, b) => a + b;
 module.exports.square = x => x * x;
+module.exports.setName = (user, fullName) => {
+  const name = fullName.split(' ');
+  return user = {
+    firstName: name[0],
+    lastName: name[1]
+  }
+};
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,20 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   const res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   const res = utils.square(5);
 
-  if (res !== 25) {
-    throw new Error(`Expected 25, but got ${res}`);
-  }
+  expect(res).toBe(25).toBeA('number');
+});
+
+it('should have first and last name', () => {
+  const userObject = {age: 21, location: 'Germany'};
+  const res = utils.setName({userObject}, 'Malle Flueh');
+  expect(res).toInclude({firstName: 'Malle', lastName: 'Flueh'}).toBeA('object');
 });
